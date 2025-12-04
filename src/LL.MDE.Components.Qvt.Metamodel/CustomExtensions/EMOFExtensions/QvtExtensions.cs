d2b3a621d4435df3323af8a9f42efb34e1c1d6f6
oalt <oa@lacheweg21.de>
2025-12-04 17:23:46 +0100

    Extension methods for EMOF access

---
+++ src/LL.MDE.Components.Qvt.Metamodel/CustomExtensions/EMOFExtensions/QvtExtensions.cs
@@ -0,0 +1,47 @@
+﻿using LL.MDE.Components.Qvt.Metamodel.QVTBase;
+using LL.MDE.Components.Qvt.Metamodel.QVTRelation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LL.MDE.Components.Qvt.Metamodel.CustomExtensions.EMOFExtensions
+{
+    public static class QvtExtensions
+    {
+        public static IEnumerable<EMOF.IPackage> GetMetaModelPackagesForTransformation(this ITransformation transformation)
+        {
+            List<EMOF.IPackage> result = new List<EMOF.IPackage>();
+
+            foreach (IRelation relation in transformation.Rule.OfType<IRelation>())
+            {
+                if(relation.IsTopLevel == true)
+                {
+                    foreach (IRelationDomain domain in relation.Domain.OfType<IRelationDomain>())
+                    {
+                        try
+                        {
+                            EMOF.IPackage domainObejctTypePackage = domain.RootVariable.Type.Package;
+
+                            EMOF.IPackage rootPackage = domainObejctTypePackage;
+
+                            while (rootPackage.NestingPackage != null)
+                            {
+                                rootPackage = rootPackage.NestingPackage;
+                            }
+
+                            if (rootPackage != null)
+                            {
+                                result.Add(rootPackage);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
