e58a41604da42b7af3977e842b61f53b936aa1be
oalt <oa@lacheweg21.de>
2026-03-26 16:42:05 +0100

    Fixing build problems and clean up solution

--- src/QvtMetamodelCodeGeneration/LL.MDE.Components.Qvt.Metamodel.Generator/Program.cs
+++ src/QvtMetamodelCodeGeneration/LL.MDE.Components.Qvt.Metamodel.Generator/Program.cs
@@ -9,18 +9,18 @@ namespace LL.MDE.Components.Qvt.Metamodel.Generator
 {
     internal class Program
     {
-        private static void GenerateCodeFor(string input, string output, string name)
-        {
-            EPackage package = EcoreInterop.LoadPackageFromFile(input);
-            INamespace nMeta = EcoreInterop.Transform2Meta(package);
-            CodeCompileUnit code = MetaFacade.CreateCode(nMeta, name);
-            MetaFacade.GenerateCode(code, new CSharpCodeProvider(), output, true);
-        }
+        //private static void GenerateCodeFor(string input, string output, string name)
+        //{
+        //    EPackage package = EcoreInterop.LoadPackageFromFile(input);
+        //    INamespace nMeta = EcoreInterop.Transform2Meta(package);
+        //    CodeCompileUnit code = MetaFacade.CreateCode(nMeta, name);
+        //    MetaFacade.GenerateCode(code, new CSharpCodeProvider(), output, true);
+        //}
 
 
         public static void Main(string[] args)
         {
-            GenerateCodeFor(@".\model\QVTRelation.ecore", @"..\..\..\..\QvtEnginePerformance\trunk\src\LL.MDE.Components.Qvt.Metamodel\Generated", "LL.MDE.Components.Qvt.Metamodel");
+            //GenerateCodeFor(@".\model\QVTRelation.ecore", @"..\..\..\..\QvtEnginePerformance\trunk\src\LL.MDE.Components.Qvt.Metamodel\Generated", "LL.MDE.Components.Qvt.Metamodel");
         }
     }
 }
